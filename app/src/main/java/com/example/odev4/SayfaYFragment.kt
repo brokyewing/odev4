2f0e3f65d02238be21cbec134e50e4b4d42ba969
brokyewing <askeroglu109@gmail.com>
2025-05-08 12:24:48 +0300

    Initial commit

---
+++ app/src/main/java/com/example/odev4/SayfaYFragment.kt
@@ -0,0 +1,27 @@
+package com.example.odev4
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.activity.OnBackPressedCallback
+import androidx.navigation.fragment.findNavController
+import com.example.odev4.databinding.FragmentSayfaYBinding
+
+
+class SayfaYFragment : Fragment() {
+    private lateinit var binding: FragmentSayfaYBinding
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        binding = FragmentSayfaYBinding.inflate(inflater, container, false)
+        requireActivity().onBackPressedDispatcher.addCallback(viewLifecycleOwner, object : OnBackPressedCallback(true) {
+            override fun handleOnBackPressed() {
+                findNavController().navigate(R.id.anasayfaFragment)
+            }
+        })
+        return binding.root
+    }
+}
